a30a97b063eb49e70dc136686ec6046b765a46f1
Aidan Kang <akang@lcs.on.ca>
2022-11-16 12:14:02 -0500

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Aidan Kang on 2022-11-16.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
