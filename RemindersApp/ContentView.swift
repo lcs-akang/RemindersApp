f2f708e518be25aea87ad2668bb5a259a6ec5309
Aidan Kang <akang@lcs.on.ca>
2022-11-18 09:38:52 -0500

    made rounded rectangle for search bar and started today box

--- RemindersApp/ContentView.swift
+++ RemindersApp/ContentView.swift
@@ -14,42 +14,77 @@ struct ContentView: View {
             Color.black
                 .ignoresSafeArea()
             
-            VStack(spacing: 20) {
+            VStack(spacing: 15) {
                 
                 HStack {
                     Spacer()
                         .frame(maxWidth: 325)
                     Text("Edit")
                         .foregroundColor(.blue)
-                        
-                        
+                    
+                    
                 }
                 ZStack {
-                    Color.yellow
+                    Color.clear
                         .frame(maxWidth: 362, maxHeight: 35)
-                        
+                        .background(
+                        RoundedRectangle(cornerRadius: 8) // Create the shape
+                        .foregroundColor(Color.yellow))
+                    
                     HStack(spacing: 5) {
                         Image(systemName:"magnifyingglass")
                             .foregroundColor(.gray)
                         Text("Search")
                             .foregroundColor(.gray)
+                            .font(.title3)
                         Spacer()
-                            .frame(maxWidth: 265)
+                            .frame(maxWidth: 255)
                         
                         
                     }
                 }
-               
+                
+                Color.clear
+                    .frame(maxHeight: 0)
+                
+                HStack() {
+                    
+                    
+                    Spacer()
+                        .frame(maxWidth: 20)
+                    
+                    ZStack {
+                        Color.clear
+                            .frame(maxWidth: 170, maxHeight: 90)
+                            .background(
+                            RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(Color.yellow))
+                        
+                        HStack {
+                            VStack {
+                                Image(systemName: "calendar.circle.fill")
+                                    .resizable()
+                                    .frame(maxWidth: 35, maxHeight: 35)
+                                Text("Today")
+                                    .bold()
+                                    
+                            }
+                            Color.clear
+                                .frame(maxWidth: 100)
+                        }
+                        
+                    }
+                }
                 Spacer()
                 
-            
+                
             }
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
