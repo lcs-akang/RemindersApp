ce74ae05eca5e61a155e9896e5c6a96e8a757fd7
Aidan Kang <akang@lcs.on.ca>
2022-11-18 10:00:56 -0500

    Completed all reminder type boxes

--- RemindersApp/ContentView.swift
+++ RemindersApp/ContentView.swift
@@ -47,11 +47,8 @@ struct ContentView: View {
                 Color.clear
                     .frame(maxHeight: 0)
                 
-                HStack() {
-                    
-                    
-                    Spacer()
-                        .frame(maxWidth: 20)
+                HStack(spacing: 18) {
+                    ReminderType(typeImage: "calendar.circle.fill", amount: "0", reminderType: "Today")
                     
                     ZStack {
                         Color.clear
@@ -60,17 +57,101 @@ struct ContentView: View {
                             RoundedRectangle(cornerRadius: 8)
                             .foregroundColor(Color.yellow))
                         
-                        HStack {
-                            VStack {
-                                Image(systemName: "calendar.circle.fill")
+                        VStack(spacing: 14) {
+                            HStack {
+                                Image(systemName: "exclamationmark.circle.fill")
                                     .resizable()
                                     .frame(maxWidth: 35, maxHeight: 35)
-                                Text("Today")
+                                
+                                Spacer()
+                                    .frame(maxWidth: 90)
+                                
+                                Text("0")
+                                    .font(.title)
                                     .bold()
-                                    
+                                
+                                Spacer()
+                                    .frame(maxWidth: 5)
+                                        
                             }
-                            Color.clear
-                                .frame(maxWidth: 100)
+                            HStack {
+                                Text("Scheduled")
+                                    .bold()
+                                Spacer()
+                                    .frame(maxWidth: 62)
+                            }
+                            
+                            
+                        }
+                        
+                    }
+                }
+                    
+                HStack(spacing: 18) {
+                    ZStack {
+                        Color.clear
+                            .frame(maxWidth: 170, maxHeight: 90)
+                            .background(
+                            RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(Color.yellow))
+                        
+                        VStack(spacing: 14) {
+                            HStack {
+                                Image(systemName: "tray.circle.fill")
+                                    .resizable()
+                                    .frame(maxWidth: 35, maxHeight: 35)
+                                
+                                Spacer()
+                                    .frame(maxWidth: 90)
+                                
+                                Text("0")
+                                    .font(.title)
+                                    .bold()
+                                
+                                Spacer()
+                                    .frame(maxWidth: 5)
+                                        
+                            }
+                            HStack {
+                                Text("All")
+                                    .bold()
+                                Spacer()
+                                    .frame(maxWidth: 120)
+                            }
+                            
+                            
+                        }
+                        
+                    }
+                    
+                    ZStack {
+                        Color.clear
+                            .frame(maxWidth: 170, maxHeight: 90)
+                            .background(
+                            RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(Color.yellow))
+                        
+                        VStack(spacing: 14) {
+                            HStack {
+                                Image(systemName: "checkmark.circle.fill")
+                                    .resizable()
+                                    .frame(maxWidth: 35, maxHeight: 35)
+                                
+                                Spacer()
+                                    .frame(maxWidth: 90)
+                                
+                                Spacer()
+                                    .frame(maxWidth: 22)
+                                        
+                            }
+                            HStack {
+                                Text("Completed")
+                                    .bold()
+                                Spacer()
+                                    .frame(maxWidth: 60)
+                            }
+                            
+                            
                         }
                         
                     }
@@ -79,6 +160,7 @@ struct ContentView: View {
                 
                 
             }
+            Spacer()
         }
     }
     
--- RemindersApp/ReminderType.swift
+++ RemindersApp/ReminderType.swift
@@ -14,17 +14,47 @@ struct ReminderType: View {
     let reminderType: String
     
     var body: some View {
-        HStack {
-            VStack {
-                Image(systemName: typeImage)
-                Text(reminderType)
+        ZStack {
+            Color.clear
+                .frame(maxWidth: 170, maxHeight: 90)
+                .background(
+                RoundedRectangle(cornerRadius: 8)
+                .foregroundColor(Color.yellow))
+            
+            VStack(spacing: 14) {
+                HStack {
+                    Image(systemName: typeImage)
+                        .resizable()
+                        .frame(maxWidth: 35, maxHeight: 35)
+                    
+                    Spacer()
+                        .frame(maxWidth: 90)
+                    
+                    Text(amount)
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                        .frame(maxWidth: 5)
+                            
+                }
+                HStack {
+                    Text(reminderType)
+                        .bold()
+                        .frame(alignment: .leading)
+                    Spacer()
+                        .frame(maxWidth: 100)
+                }
+                
+                
             }
+            
         }
     }
 }
 
 struct ReminderType_Previews: PreviewProvider {
     static var previews: some View {
-        ReminderType(typeImage: "globe", amount: "4", reminderType: "Today")
+        ReminderType(typeImage: "calendar.circle.fill", amount: "4", reminderType: "Today")
     }
 }
